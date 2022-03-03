fa57912b1fbb928a4dd12204775c286233f09034
sangdeok <leesangdeok84@gmail.com>
2022-03-03 14:09:09 +0900

    Delete run_consul_agent.sh

--- consul/run_consul_agent.sh
+++
@@ -1,6 +0,0 @@
-if [ "$1" = "cluster" ]
-then
-        /usr/local/bin/consul agent -config-file=/home/consul/consul_c1.json -log-file=/home/consul/logs/ &
-else
-        /usr/local/bin/consul agent -config-file=/home/consul/consul_standalone.json -log-file=/home/consul/logs_standalone/ &
-fi
